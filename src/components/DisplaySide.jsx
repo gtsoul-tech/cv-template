11860465bba834533ba8b6d0a5df4146e8520368
gtsoul-tech <56584633+gtsoul-tech@users.noreply.github.com>
2023-12-17 17:21:43 +0200

    add layout handle

--- src/components/DisplaySide.jsx
+++ src/components/DisplaySide.jsx
@@ -3,16 +3,44 @@ import EducationDetails from './EducationDetails';
 import ExperienceDetails from './ExperienceDetails';
 import '../styles/DisplaySide.css'
 import PropTypes from 'prop-types';
-function DisplaySide({person,degrees,experiences}) {
-    //props
+function DisplaySide({person,degrees,experiences,fontColorLayout}) {
+  const class1 = 'font-face-didot'
+
   return (
     <>
-      <div className='displaySide'>
-        
+    <div className={`displaySide ${class1}`}>
+      {fontColorLayout.layout === 'top' ?
+      <div className={`top`}>
         <PersonalDetails person={person}></PersonalDetails>
         <EducationDetails degrees={degrees}></EducationDetails>
         <ExperienceDetails experiences={experiences}></ExperienceDetails>
+      </div>
+      :
+      fontColorLayout.layout=== 'left' ? 
+      <div className={`left`}>
+        <div className='displayComponentHeader'>
+          <PersonalDetails person={person}></PersonalDetails>
+        </div>
+        <div className='displayComponent'>
+          <EducationDetails degrees={degrees}></EducationDetails>
+          <ExperienceDetails experiences={experiences}></ExperienceDetails>
+        </div>
+      </div>
+      
+      :
+      fontColorLayout.layout=== 'right' ? 
+      <div className={`right`}>
         
+        <div className='displayComponent'>
+          <EducationDetails degrees={degrees}></EducationDetails>
+          <ExperienceDetails experiences={experiences}></ExperienceDetails>
+        </div>
+        <div className='displayComponentHeader'>
+          <PersonalDetails person={person}></PersonalDetails>
+        </div>
+      </div>
+      :
+      null}
       </div>
     </>
   )
@@ -20,6 +48,7 @@ function DisplaySide({person,degrees,experiences}) {
 DisplaySide.propTypes = {
     person: PropTypes.object,
     degrees:PropTypes.array,
-    experiences:PropTypes.array
+    experiences:PropTypes.array,
+    fontColorLayout:PropTypes.object,
 }
 export default DisplaySide
--- src/components/EditSide.jsx
+++ src/components/EditSide.jsx
@@ -3,24 +3,94 @@ import '../styles/EditSide.css'
 import PersonalForm from './PersonalForm'
 import EducationForm from './EducationForm';
 import ExperienceForm from './ExperienceForm';
+import { useState } from 'react';
 function EditSide(props) {
+  const [show,setShow]= useState(true)
+  const mystyle = {
+    backgroundColor: props.fontColorLayout.color,
+    fontFamily: props.fontColorLayout.font
+  };
   return (
     <>
+      
+      
       <div className='editSide'>
         <div className='buttonInit'>
-        <button onClick={() => props.handleLoadExample()}>Load Example</button>
-        <button className='buttonInitClear' onClick={() => props.handleClear()}>Clear Example</button>
+          <button onClick={() => setShow(!show)}>Resume/Customize</button>
         </div>
-        
-        <PersonalForm {...props}></PersonalForm>
-        <EducationForm {...props}></EducationForm>
-        <ExperienceForm {...props}></ExperienceForm>
+        {show ? <>
+          <div className='buttonInit'>
+            <button onClick={() => props.handleLoadExample()}>Load Example</button>
+            <button className='buttonInitClear' onClick={() => props.handleClear()}>Clear Example</button>
+          </div>
+          <PersonalForm {...props}></PersonalForm>
+          <EducationForm {...props}></EducationForm>
+          <ExperienceForm {...props}></ExperienceForm>
+        </>: 
+        <>
+          <div className='card'>
+            <div className='cardTitle'>Layout</div>
+            <div className='cardComponents'>
+              <button className='layoutButtonTop'
+              onClick={()=> {props.handleSetFontColorLayout(props.fontColorLayout.font,props.fontColorLayout.color,'top')}}
+              >
+                <div style={mystyle}></div>
+                <div></div>
+              </button>
+              <button className='layoutButtonLeft'
+              onClick={()=> {props.handleSetFontColorLayout(props.fontColorLayout.font,props.fontColorLayout.color,'left')}}
+              >
+                <div style={mystyle}></div>
+                <div></div>
+              </button>
+              <button className='layoutButtonRight'
+              onClick={()=> {props.handleSetFontColorLayout(props.fontColorLayout.font,props.fontColorLayout.color,'right')}}
+              >
+                <div></div>
+                <div style={mystyle}></div>
+              </button>
+            </div>
+          </div>
+          <div className='card'>
+            <div className='cardTitle'>Color</div>
+            <div className='cardComponents'>
+              <div>Change Color</div>
+              <input type="color" id="colorpicker" value={props.fontColorLayout.color} onChange={(e)=> {props.handleSetFontColorLayout(props.fontColorLayout.font,e.target.value,props.fontColorLayout.layout)}}></input>
+            </div>
+          </div>
+          <div className='card'>
+            <div className='cardTitle'>Font</div>
+            <div className='cardComponents'>
+              <button
+              onClick={()=> {props.handleSetFontColorLayout('sans',props.fontColorLayout.color,props.fontColorLayout.layout)}}
+              >
+                <div>Aa</div>
+                <div>Sans</div>
+              </button>
+              <button
+              onClick={()=> {props.handleSetFontColorLayout('sans',props.fontColorLayout.color,props.fontColorLayout.layout)}}
+              >
+                <div>Aa</div>
+                <div>Sans</div>
+              </button>
+              <button
+              onClick={()=> {props.handleSetFontColorLayout('sans',props.fontColorLayout.color,props.fontColorLayout.layout)}}
+              >
+                <div>Aa</div>
+                <div>Sans</div>
+              </button>
+            </div>
+          </div>
+        </>}
       </div>
+      
     </>
   )
 }
 EditSide.propTypes = {
   handleLoadExample:PropTypes.func,
   handleClear:PropTypes.func,
+  fontColorLayout:PropTypes.object,
+  handleSetFontColorLayout:PropTypes.func,
 }
 export default EditSide
